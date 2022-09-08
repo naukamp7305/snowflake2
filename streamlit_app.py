dce3ed6d5f4893d017167f1d5d865ba5ac0b9771
naukamp7305 <113120851+naukamp7305@users.noreply.github.com>
2022-09-08 16:51:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
 streamlit.title('Demo')
 streamlit.header('Brakfast menu')
 streamlit.text('Omega')
 streamlit.text('Kale')
 streamlit.text('Hard')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
